9bf0cd4f34df06fccb7151c36c13e439ee5573a8
avinash1810 <42260682+avinash1810@users.noreply.github.com>
2018-09-24 15:43:27 +0530

    Create leap year.java

---
+++ leap year.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class JAVA
+{
+	public static void main (String[] args)
+	{
+	
+    int leapYear = 1900; 
+
+    if (leapYear <= 2020){
+        for (int x = 0; x <= 30; x++){ 
+            System.out.print(leapYear + " ");
+            leapYear = leapYear + 4;
+        }
+        System.out.println(" ");
+        int x = 0;
+    } 
+    }
+}
