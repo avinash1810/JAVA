2c8ee5f792b02654070a54e8547bf3b29a535d9b
avinash1810 <42260682+avinash1810@users.noreply.github.com>
2018-09-24 15:49:11 +0530

    Create if elase.java

---
+++ if elase.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=60;
+      if(a<50)
+      {
+        System.out.println("box office hit");
+      }
+      else
+      {
+        System.out.println("flop");
+      }
+	}
+}
