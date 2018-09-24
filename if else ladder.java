f386a59c9e8dee8ba2ba330e0a5513a5c1d71ccf
avinash1810 <42260682+avinash1810@users.noreply.github.com>
2018-09-24 15:50:26 +0530

    Create if else ladder.java

---
+++ if else ladder.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+      int rs=20000;
+      if(rs>22000)
+      {
+        System.out.println("free trolley suitcase");
+      }
+      else if(rs>10000)
+      {
+        System.out.println("free travel bag");
+      }
+      else if(rs>5000)
+      {
+        System.out.println("free backpack");
+      }
+      else(rs<5000)
+      {
+        System.out.print("collect money");
+      }
+	}
+}
