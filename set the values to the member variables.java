9637d81b89493ae4f0ffe6f7d50354975c61a58b
avinash1810 <42260682+avinash1810@users.noreply.github.com>
2018-09-25 14:01:29 +0530

    Create  set the values to the member variables.java

---
+++ set the values to the member variables.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Github
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      public class Bicycle {
+        
+    private int cadence;
+    private int gear;
+    private int speed;
+        
+    public Bicycle(int startCadence, int startSpeed, int startGear) {
+        gear = startGear;
+        cadence = startCadence;
+        speed = startSpeed;
+    }
+        
+    public int getCadence() {
+        return cadence;
+    }
+        
+    public void setCadence(int newValue) {
+        cadence = newValue;
+    }
+        
+    public int getGear() {
+        return gear;
+    }
+        
+    public void setGear(int newValue) {
+        gear = newValue;
+    }
+        
+    public int getSpeed() {
+        return speed;
+    }
+        
+    public void applyBrake(int decrement) {
+        speed -= decrement;
+    }
+        
+    public void speedUp(int increment) {
+        speed += increment;
+    }
+}
+	}
+}
