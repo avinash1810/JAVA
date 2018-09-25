348cf4d7ac8488aadc784275851478715d690f83
avinash1810 <42260682+avinash1810@users.noreply.github.com>
2018-09-25 14:08:05 +0530

    Create zip code.java

---
+++ zip code.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Github
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+      int zip = 123;
+		DecimalFormat format = new DecimalFormat("00000");
+		System.out.println(format.format(zip));	
+      System.out.println("chennai");
+      
+	}
+}
